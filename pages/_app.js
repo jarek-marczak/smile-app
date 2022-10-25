9e7b0bc2a6947dccf5677aa35e8d4e625350dae4
Jarek Marczak <jarek.marczak@outlook.com>
2022-10-25 15:43:08 +0100

    added starting project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { SessionProvider } from 'next-auth/react';
+
+import Layout from '../components/layout/layout';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // `session` comes from `getServerSideProps` or `getInitialProps`.
+    // Avoids flickering/session loading on first load.
+    <SessionProvider session={session} refetchInterval={5 * 60}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
